97b885d734872b123c3874774b7762d05a7d1f91
Qirui <huangqirui@gmail.com>
2026-01-05 23:22:50 +0800

    add make fivecardplays comparable

--- src/main/java/big2/FiveCardPlay.java
+++ src/main/java/big2/FiveCardPlay.java
@@ -3,8 +3,10 @@ package big2;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class FiveCardPlay {
+public class FiveCardPlay implements Comparable<FiveCardPlay>{
     private final Card[] cards;
+    private final Card topCard;
+    private final int rank;
 
     static boolean isStraight(Card[] cards){
         Arrays.sort(cards, Comparator.comparingInt(Card::getRawValue));
@@ -58,9 +60,35 @@ public class FiveCardPlay {
         }
         if(isStraight(cards) || isFlush(cards) || isFullHouse(cards) || isQuad(cards)){
             this.cards = cards;
+            if(isStraight(cards) && isFlush(cards)){
+                this.rank = 1000000;
+                // isStraight has caused the cards to be sorted in raw order which is correct for comparing straights
+                this.topCard = cards[4];
+            }
+            else if(isStraight(cards)){
+                this.rank = 1;
+                this.topCard = cards[4];
+            }
+            else if(isFlush(cards)){
+                Arrays.sort(cards);
+                this.rank = 10;
+                this.topCard = cards[4];
+            }
+            else if(isFullHouse(cards)){
+                this.rank = 20;
+                this.topCard = cards[2];
+            }
+            else if(isQuad(cards)){
+                this.rank = 30;
+                this.topCard = cards[2];
+            }
+            else throw new RuntimeException("Error creating FiveCardPlay");
         }
         else throw new InvalidPlayException("Not a valid set of 5 cards");
     }
 
-
+    @Override
+    public int compareTo(FiveCardPlay o) {
+        return 0;
+    }
 }
