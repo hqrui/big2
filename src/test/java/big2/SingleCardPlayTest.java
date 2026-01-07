5d29928a427aa7996f9c6d24bd4ba90d8a1a7876
Qirui <huangqirui@gmail.com>
2026-01-07 14:59:34 +0800

    add test dir

---
+++ src/test/java/big2/SingleCardPlayTest.java
@@ -0,0 +1,14 @@
+package big2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleCardPlayTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+    }
+}
