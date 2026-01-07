228aeb2407723189be01263ef378f2c3516b1adf
haoren-zhou <hrz9009@gmail.com>
2026-01-07 17:28:16 +0800

    test: add testcases for SingleCardPlay

--- src/test/java/big2/SingleCardPlayTest.java
+++ src/test/java/big2/SingleCardPlayTest.java
@@ -4,19 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SingleCardPlayTest {
 
-    private Card c1, c2, c3;
+  private Card c1, c2, c3, c4, c5, c6;
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-        c1 = Card.fromAbbrev("3D");
-        c2 = Card.fromAbbrev("2H");
-        c3 = Card.fromAbbrev("JC");
-    }
+  @org.junit.jupiter.api.BeforeEach
+  void setUp() {
+    c1 = Card.fromAbbrev("3D");
+    c2 = Card.fromAbbrev("2H");
+    c3 = Card.fromAbbrev("JD");
+    c4 = Card.fromAbbrev("JC");
+    c5 = Card.fromAbbrev("JH");
+    c6 = Card.fromAbbrev("JS");
+  }
 
-    @org.junit.jupiter.api.Test
-    void compareTo() {
-        assertEquals(-1, c1.compareTo(c2));
-        assertEquals(-1, c1.compareTo(c3));
-        assertEquals(1, c2.compareTo(c3));
-    }
+  @org.junit.jupiter.api.Test
+  void compareTo() {
+    assertEquals(-1, c1.compareTo(c2));
+    assertEquals(-1, c1.compareTo(c3));
+    assertEquals(1, c2.compareTo(c3));
+
+    // compare suits
+    assertEquals(-1, c3.compareTo(c4));
+    assertEquals(1, c4.compareTo(c3));
+    assertEquals(-1, c3.compareTo(c5));
+    assertEquals(1, c6.compareTo(c5));
+  }
 }
